15600ae58df69b12325e499098375e9ea6675145
Joel Smith <jsumnersmith@gmail.com>
2023-11-08 22:36:31 -0500

    Small updates to search bar

--- components/Search.js
+++ components/Search.js
@@ -20,11 +20,11 @@ export default function Search({initialSearch = ''}) {
     <div className="flex gap-3 my-6 items-center w-full">
       <input 
         type="text"
-        placeholder="Search by song or artist name"
+        placeholder="Search by song, artist name, or lyrics"
         value={search}
         onChange={handleChange}
         onKeyPress={handleEnter}
-        className="button-shadow px-3 py-2 flex-grow rounded-md" 
+        className="border-cool-gray-200 border px-3 py-2 flex-grow rounded-md" 
       /> 
       <Link href={`/search?q=${search}`} className="button-shadow bg-white hover:bg-opacity-90 focus:border-blue-600 focus:ring-0 px-4 py-2 text-md rounded-md flex gap-2 items-center"><SearchIcon height={20}/>Search</Link>
     </div>
